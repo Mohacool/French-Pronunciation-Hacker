c5fba64491eded3d5ea2a9af732ee96eb750505b
Mohasalama <msalama2007@hotmail.com>
2021-02-02 20:52:23 -0500

    mailing

--- app.js
+++ app.js
@@ -39,6 +39,9 @@ const User = require('./model/user')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
+// Node mailer
+const nodemailer = require("nodemailer");
+
 const ObjectId = require('mongodb').ObjectId;
 
 const jwt_secret = process.env.JWT_SECRET
@@ -166,6 +169,39 @@ app.post('/api/register', async (req,res) =>{
         // throw error
     }
     res.json({status:'ok'})
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Feb 2 mailing~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    let testAccount = await nodemailer.createTestAccount();
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "mail.pronunciationhacker.a2hosted.com",
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: 'french@pronunciationhacker.a2hosted.com', 
+        pass: process.env.SERVER_PASSWORD
+      },
+      tls:{
+        rejectUnauthorized:false
+      }
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: 'pronunciationhacker.a2hosted.com', // sender address
+      to: "moha.salama@mail.utoronto.ca", // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
     
 
 
